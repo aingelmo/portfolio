0fc4574e137ca18264b68dcf94663b9fb2b7fa54
Andrés Ingelmo <andrw333@gmail.com>
2021-11-22 11:01:14 +0100

    Added test line

--- covid_heroku/main.py
+++ covid_heroku/main.py
@@ -7,6 +7,8 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 
+# Deploy from github
+
 # -------------------------------------------------------------------------------------------------
 # Define key variables
 url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
