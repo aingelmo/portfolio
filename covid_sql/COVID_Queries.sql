9c67058202adb2435a907a207046f671f08258f8
Andrés Ingelmo <andrw333@gmail.com>
2021-11-30 12:32:33 +0100

    Added multiple queries

--- covid_sql/COVID_Queries.sql
+++ covid_sql/COVID_Queries.sql
@@ -17,6 +17,33 @@ WHERE t1.continent IS NOT null
 GROUP BY t1.location
 ORDER BY new_cases DESC
 
+-- Get the worst day of the covid total cases with CTE
+--- Define the CTE expression name and column list
+WITH newCases_country (location, date, new_cases) 
+AS
+--- Define the CTE query
+(
+	SELECT
+		t1.location AS location,
+		MAX(t1.date) AS date,
+		MAX(t1.new_cases) AS new_cases
+	FROM covid_data AS t1
+	INNER JOIN (
+		SELECT
+			location,
+			MAX(new_cases) AS new_cases
+		FROM covid_data
+		WHERE new_cases IS NOT null AND continent IS NOT null
+		GROUP BY location
+	) AS t2
+		ON t1.location = t2.location AND t1.new_cases = t2.new_cases
+	WHERE t1.continent IS NOT null
+	GROUP BY t1.location
+)
+--- Define the outer query referencing the CTE name
+SELECT *
+FROM newCases_country
+
 -- Get the worst day of the covid new cases measured by new cases per million
 SELECT
 	t1.location AS location,
@@ -65,7 +92,7 @@ WHERE continent IS NOT null	AND people_fully_vaccinated IS NOT null
 GROUP BY location
 ORDER BY percentage_fully_vaccinated DESC
 
---Percentage of people who died and the country life expectancy
+-- Percentage of people who died and the country life expectancy
 SELECT
 	location,
 	MAX(life_expectancy) AS life_expectancy,
@@ -75,7 +102,18 @@ WHERE continent IS NOT null	AND life_expectancy IS NOT null AND population IS NO
 GROUP BY location
 ORDER BY percentage_died DESC
 
---Top days where new cases were reported (Worldwide)
+-- Create a view of the last query to use in future analysis
+CREATE VIEW mortalityRate_lifeExpectancy AS
+SELECT
+	location,
+	MAX(life_expectancy) AS life_expectancy,
+	SUM(new_deaths) / MAX(population) * 100 AS percentage_died
+FROM covid_data
+WHERE continent IS NOT null	AND life_expectancy IS NOT null AND population IS NOT null AND new_deaths IS NOT null
+GROUP BY location
+ORDER BY percentage_died DESC
+
+-- Top days where new cases were reported (Worldwide)
 SELECT
 	date,
 	SUM(new_cases) AS new_cases
@@ -83,3 +121,35 @@ FROM covid_data
 WHERE continent IS NOT null AND new_cases IS NOT null
 GROUP BY date
 ORDER BY new_cases DESC 
+
+-- Get correlation coefficient between percentage of smoking population and deaths
+SELECT CORR(smokers, deaths_per_100000)
+FROM
+	(SELECT
+		location,
+		(MAX(female_smokers) + MAX(male_smokers))/2 AS smokers,
+		MAX(total_deaths) / MAX(population) * 100000 AS deaths_per_100000
+	FROM covid_data
+	WHERE male_smokers IS NOT null AND continent IS NOT null AND total_deaths IS NOT null
+	GROUP BY location) AS t
+
+-- Get correlation coefficient between percentage of smoking population and deaths without a builtin function
+SELECT
+	((tot_sum - (smokers_sum * deaths_per_100000_sum / _count)) / SQRT((smokers_sum_sq - pow(smokers_sum, 2.0) / _count) * (deaths_per_100000_sum_sq - pow(deaths_per_100000_sum, 2.0) / _count))) AS pearson_corr
+FROM(
+	SELECT
+		SUM(smokers) AS smokers_sum,
+		SUM(deaths_per_100000) AS deaths_per_100000_sum,
+		SUM(smokers * smokers) AS smokers_sum_sq,
+		SUM(deaths_per_100000 * deaths_per_100000) AS deaths_per_100000_sum_sq,
+		SUM(smokers * deaths_per_100000) AS tot_sum,
+		COUNT(*) AS _count
+	FROM
+		(SELECT
+			location,
+			(MAX(female_smokers) + MAX(male_smokers))/2 AS smokers,
+			MAX(total_deaths) / MAX(population) * 100000 AS deaths_per_100000
+		FROM covid_data
+		WHERE male_smokers IS NOT null AND continent IS NOT null AND total_deaths IS NOT null
+		GROUP BY location) AS t1
+	) AS t2
